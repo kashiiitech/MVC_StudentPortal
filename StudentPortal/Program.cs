de7272c7e77ca4c21fe2e8ce82b5f3ed80d7d039
kashiiitech <kashiiitech@gmail.com>
2025-01-29 23:34:35 +0500

    Injected DbContext and added ConnectionString

--- StudentPortal/Program.cs
+++ StudentPortal/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Inject DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal"));
+});
 
 var app = builder.Build();
 
