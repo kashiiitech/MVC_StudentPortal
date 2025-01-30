7332e1cfbd4c3f37352bd8f81367f14b0eb238f3
kashiiitech <kashiiitech@gmail.com>
2025-01-30 18:34:55 +0500

    Add Student View and Controller added

---
+++ StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
