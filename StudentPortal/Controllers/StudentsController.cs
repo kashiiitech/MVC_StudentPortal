1b5892bd8a826a5f94aba50922d1270f0dd145fb
kashiiitech <kashiiitech@gmail.com>
2025-01-31 10:26:24 +0500

    Added student to Database

--- StudentPortal/Controllers/StudentsController.cs
+++ StudentPortal/Controllers/StudentsController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
 using StudentPortal.Models;
+using StudentPortal.Models.Entities;
 
 namespace StudentPortal.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -12,8 +21,21 @@ namespace StudentPortal.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(AddStudentViewModel viewModel)
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
         {
+            // student entity
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed,
+            };
+
+            await dbContext.Students.AddAsync(student);
+
+            await dbContext.SaveChangesAsync();
+
             return View();
         }
     }
